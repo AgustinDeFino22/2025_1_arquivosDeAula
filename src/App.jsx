7b8c63531d45881d794f9302c3bc7f1610706ec5
Agustin <agustindefino19@gmail.com>
2025-02-08 00:13:23 -0300

    criando componentes e algumas pastas importantes

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Body from './componentes/Body'
+import Demo1 from './componentes/Demo1'
+import Header from './componentes/Header'
+
+function App() {
+
+  return (
+    <>
+        <Header />
+        <Body />
+        <Demo1 />
+    </>
+  )
+}
+
+export default App
---
+++ src/componentes/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='container-header'>
+      <p>Logo</p>
+      <p>Link1</p>
+      <p>Link2</p>
+      <p>Link3</p>
+    </div>
+  )
+}
+
+export default Header
